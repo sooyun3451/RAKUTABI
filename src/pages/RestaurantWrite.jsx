cecdc76ff68111591c8b7d4bcec5dc37170426ab
kimguenwoo <jsa03233@naver.com>
2025-08-22 17:19:44 +0900

    feature/raku_home

--- src/pages/RestaurantWrite.jsx
+++ src/pages/RestaurantWrite.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function Restaurant_Write() {
-  return <div>Restaurant_Write</div>;
+export default function RestaurantWrite() {
+  return <div>RestaurantWrite</div>;
 }
