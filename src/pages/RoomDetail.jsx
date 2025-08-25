e846ea5611a1ebf7c6fedbfed8f73400645f1b9d
tttt0817 <tttt0817@naver.com>
2025-08-25 14:14:18 +0900

    feat

--- src/pages/RoomDetail.jsx
+++ src/pages/RoomDetail.jsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React from "react";
 
 export default function RoomDetail() {
-  return <div>RoomDetail</div>;
+    return (
+        <div className="room-detail">
+            <div className="data-component"></div>//사진
+            <div className="provider-information-contentbox">
+              <div className="tree"></div>//3개
+              <div className=""></div>
+            </div>
+        </div>
+    );
 }
