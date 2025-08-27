87354d85e4ecbb2eb0a11e28a6d9403d0e51a215
jongw0o0 <jwlee6861@gmail.com>
2025-08-27 18:45:32 +0900

    room_list2 중간 저장

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react'
 
 import '../css/list_item.css'
 
@@ -6,6 +6,13 @@ export default function ListItem({hotel}) {
 
   const room1 = hotel.room1
   const room2 = hotel.room2
+
+  const review1 = hotel.review1
+  const review2 = hotel.review2
+
+  const totalScore = 5
+  const [starPoint, setStarPoint] = useState((review1[0].score + review2[0].score)/2);
+
 
   return(
     <li className='ListItem'>
@@ -17,19 +24,26 @@ export default function ListItem({hotel}) {
         </div>
         <div className='right'>
           <div className='info'>
-            <p>점</p>
-            <p>{hotel.hotelName}</p>
-            <p>{hotel.city}</p>
+            <p className='stars'>  
+              {[...Array(totalScore)].map((_, i) => 
+                  <span key={i}>
+                      {i < starPoint ? '★':''}
+                  </span>
+              )}
+            </p>
+            <p className='hotelName'>{hotel.hotelName}</p>
+            <p className='hotelCity'><span><img src='/images/ping.png' alt='ping'/></span>{hotel.city}</p>
             {hotel.totalRoomNumber < 100 &&
-              <p>잔여 객실 : {hotel.totalRoomNumber}개</p>
+              <p className='leftRoom'>잔여 객실 : {hotel.totalRoomNumber}개</p>
             }
           </div>
           <div className='price'>
-            <p>{room1[0].price <= room2[0].price ? room1[0].price : room2[0].price} ~</p>
+            <p className='roomPrice'>{room1[0].price <= room2[0].price ? room1[0].price.toLocaleString('ko-KR', { style: 'currency', currency: 'KRW' }) : room2[0].price.toLocaleString('ko-KR', { style: 'currency', currency: 'KRW' })} ~</p>
+            <p>객실당</p>
           </div>
         </div>
       </div>
-      <div>
+      <div className='more'>
         <button>자세히 보기</button>
       </div>
     </li>
--- src/pages/RoomList.jsx
+++ src/pages/RoomList.jsx
@@ -4,46 +4,115 @@ import '../css/room_list.css';
 import axios from 'axios';
 
 
+const SORT_OPTION_LIST = [
+  {value : 'scoreTop', name :'평점 높은 순'},
+  {value : 'scoreDown', name :'평점 낮은 순'},
+  {value : 'priceTop', name :'높은 가격 순'},
+  {value : 'priceDown', name :'낮은 가격 순'}
+];
+
+
+
 export default function RoomList() {
-
+  const [isReday, setIsReady] = useState(false);
   const [hotels, setHotels] = useState([]);
+  const [hotelData, setHotelData] = useState(hotels);
 
-  const [price, setPrice] = useState(100000);
-  const [minPrice, setMinPrice] = useState(10000); // 최저가
-  const [maxPrice, setMaxPrice] = useState(1000000); // 최고가
+  const mapfn = (arg) => Number(arg);
+  const room1Price = hotels.map(hotel => hotel.room1[0].price);
+  const r1 = String(room1Price);
+  const r1P = r1.split(',').map(mapfn);
+  const room2Price = hotels.map(hotel => hotel.room2[0].price);
+  const r2 = String(room2Price);
+  const r2P = r2.split(',').map(mapfn);
+  const roomPrice = r1P.concat(r2P);
+
+  //hotels에서 room1 room2 가격의 최대값 찾기
+  const maxRoomPrice = Math.max(...roomPrice);
+
+  //hotels에서 room1 room2 가격의 최소값 찾기
+  const minRoomPrice = Math.min(...roomPrice);
+
+  const [currentSort, setCurrentSort] = useState(SORT_OPTION_LIST[0].value);
+  
+  const [price, setPrice] = useState();
+  const [minPrice, setMinPrice] = useState(minRoomPrice); // 최저가
+  const [maxPrice, setMaxPrice] = useState(maxRoomPrice); // 최고가
+  const handleChangeCurrentSort = e => setCurrentSort(e.target.value);
   const handleChangePrice = e => setPrice(e.target.value);
+  
+  
   const handleChangeMinPrice = e => setMinPrice(e.target.value);
   const handleChangeMaxPrice = e => setMaxPrice(e.target.value);
+
+
 
   useEffect(() => {
     fetchData()
   }, []);
 
+  // 데이터 로드 함수
   const fetchData = async () => {
     try{
       const res = await axios.get('/data/room.json');
-      setHotels(res.data)
+      setHotels(res.data);
     }catch(e){
-      console.error(e)
+      console.error(e);
+    } finally {
+      setIsReady(true);
     }
   }
+
+  
+  
+
+  // 필터!!!!!!!!!!!!!!!!!!
+  const getFilterChange = () => {
+    const data = hotels;
+    const sortList = data.sort((a, b) => {
+        if(currentSort === 'scoreTop') {
+          return ;
+        } else if(currentSort === 'scoreDown') {
+          return ;
+        } else if(currentSort === 'priceTop') {
+          return ;
+        } else if(currentSort === 'priceDown') {
+          return ;
+        }
+    });
+    return sortList
+  }
+  const filterSortList = getFilterChange();
+
+  
+  // 1박당 요금 슬라이더
+  const handlePrice = () => {
+    // const aa = hotels.filter(hotel => hotel.room1[0].price <= maxRoomPrice && hotel.room2[0].price <= maxRoomPrice);
+    const aa = hotels.filter(hotel => hotel.room1[0].price <= price && hotel.room2[0].price <= price);
+    setHotelData(aa);
+    setHotels(aa);
+  }
+
 
   return(    
     <div id='RoomList'>
       <div className='top'>
-        <select className="roomOpt">
-          <option value="scoreTop">평점 높은 순</option>
-          <option value="scoreDown">평점 낮은 순</option>
-          <option value="priceTop">높은 가격 순</option>
-          <option value="priceDown">낮은 가격 순</option>
+        <select 
+          className="roomOpt"
+          value={currentSort}
+          onChange={handleChangeCurrentSort}
+        >
+          {SORT_OPTION_LIST.map((opt, idx) => 
+            <option key={idx} value={opt.value}>{opt.name}</option>
+          )}
         </select>
         <div className='priceRange'>
           <p>1박당 요금</p>
           <div>
             <input 
               type='range'
-              min={minPrice} // 최저가
-              max={maxPrice} // 최고가
+              min={minRoomPrice} // 최저가
+              max={maxRoomPrice} // 최고가
               value={price}
               onChange={handleChangePrice}
               className='priceRangeFilter'
@@ -52,7 +121,7 @@ export default function RoomList() {
           <div className='rangeResult'>
             <input 
               type='text'
-              value={minPrice}
+              value={minRoomPrice}
               onChange={handleChangeMinPrice}
 
             />
@@ -61,15 +130,15 @@ export default function RoomList() {
               value={price}
               onChange={handleChangeMaxPrice}
             />
-            <button>검색</button>
+            <button onClick={handlePrice}>검색</button>
           </div>
         </div>
       </div>
       <div className='bottom'>
-        <p className='total'>총 {hotels.length}건</p> 
-        <ul>
-          {hotels.map(hotel => <ListItem key={hotel.hotelId} hotel={hotel}/>)}
-        </ul>
+        <p className='total'>총 {hotels.length}건</p>
+        <div>
+          {hotels.map(hotel => <ListItem key={hotel.hotelId} hotel={hotel} />)}
+        </div>
       </div>
     </div>
   );
